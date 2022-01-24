f8c194cbd68b3067012738c0a1aabcd218622e15
tal farkash <tal_farkash@walla.com>
2022-01-24 20:18:48 +0200

    test master

--- CTools/Header.jsx
+++ CTools/Header.jsx
@@ -1,30 +1,18 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
-import { heart_icon } from '../images/headerLogo/heart.png';
+
 
 export default function Header(props) {
-  const { logo_image, title, flex, paddingRight } = props
+  const { logo_image, title, flex,paddingRight } = props
 
-  const get_icon = (value) => {
-    switch (value) {
-      case 'heart':
-        return heart_icon;
-      default:
-        break;
-    }
-  }
   return (
     <>
       <View style={styles.container(flex)}>
         <Text style={styles.title(paddingRight)}>{title}</Text>
-<<<<<<< HEAD
-        
-        {/* <Image
-=======
+
         <Image
->>>>>>> tal
           style={styles.Image}
-          source={get_icon(logo_image)}
+          source={logo_image ? require('../images/headerLogo/heart.png'):''}
         />
 
       </View>
@@ -46,13 +34,13 @@ const styles = StyleSheet.create({
     height: '84%',
     width: '25%',
   },
-  title: (paddingRight = 0) => {
+  title: (paddingRight=0) => {
     return {
       fontSize: 30,
       width: '70%',
       height: '55%',
       textAlign: 'right',
-      paddingRight: paddingRight + '%',
+      paddingRight: paddingRight+'%',
       fontWeight: 'bold',
       color: 'white',
       textShadowColor: '#1EA6D6',
