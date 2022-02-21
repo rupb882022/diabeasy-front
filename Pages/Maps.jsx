624cc1be777567ef44a91133a986668d75f26fe0
idanlavee <idanlavee10@gmail.com>
2022-02-21 18:24:33 +0200

    fix sign in and continue Maps page

--- Pages/Maps.jsx
+++ Pages/Maps.jsx
@@ -1,17 +1,30 @@
-import { StyleSheet, Text, View,Dimensions } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View,Dimensions, Alert, Vibration } from 'react-native'
+import React, { useEffect, useState,useRef } from 'react'
 import MapView, { Callout, Circle, Marker ,PROVIDER_GOOGLE,OverlayComponent} from 'react-native-maps'
 import { Entypo } from '@expo/vector-icons'; 
 import Header from '../CTools/Header';
 import Input from '../CTools/Input';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+
 
 export default function Maps() {
+   const [distance,setDistance]= useState(0)
+
   const [pin,setPin]=useState({
     latitude: 32.34245547297243,
    longitude:34.911549397360595
    })
 
+   const [region,setRegion] = useState({
+		latitude: 32.34245547297243,
+		longitude: 34.911549397360595,
+    latitudeDelta: 0.0125,
+    longitudeDelta: 0.0121,
+   })
 
+   
+  
   return (
 <>
 
@@ -24,30 +37,73 @@ export default function Maps() {
    //image_margin={{ Bottom: -4}}
    //possiton={70}
    marginLeft={10}
+   
    justifyContent='flex-start'/>
-{/* <Input
-placeholder='Search'/> */}
 
 
-<MapView loadingEnabled={true}
-style={{flex: 0.7,marginBottom:'10%'}}
- region={{
+<GooglePlacesAutocomplete
+      placeholder='Search'
+      autoFocus={true}
+      fetchDetails={true}
+     // listViewDisplayed='auto'    // true/false/undefined
+      GooglePlacesSearchQuery={{
+        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+        rankby: 'distance',
+        types:'gym', //'food',
+      }}      
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+        setRegion({
+          latitude: details.geometry.location.lat,
+          longitude: details.geometry.location.lng,
+          latitudeDelta:  0.0125,
+          longitudeDelta: 0.0121,
+        });
+      }}
+      query={{
+        key: 'AIzaSyCV-2W1x_rZGIM8ZyvYpnFu6A5J_OQFyhU',
+        language: 'en',
+        //components: "country:us",
+				//	types: "establishment",
+					radius: 10000,
+					location: `${region.latitude}, ${region.longitude}`
+      }}
+      styles={{container:{flex:0,position:'absolute',top:'9%',width:'100%',zIndex:1}}}
+     // currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+      //currentLocationLabel="Current location"
+      //nearbyPlacesAPI="GooglePlacesSearch"
+     
+    />
+<MapView 
+loadingEnabled={true}
+style={{flex: 0.9,marginBottom:'16%'}}
+initialRegion={{
   latitude: 32.34245547297243,
   longitude:34.911549397360595 ,
- latitudeDelta: 0.0122,
+ latitudeDelta: 0.0125,
  longitudeDelta: 0.0121,
 }}
-provider={PROVIDER_GOOGLE}  //--> By delete this line, the maps provider will be Apple
+showsUserLocation={true}
+showsMyLocationButton={true}
+nearbyPlacesAPI="GooglePlacesSearch"
+//onRegionChangeComplete={(region)=>{setRegion(region)}}
+
+provider='google'  //--> By delete this line, the maps provider will be Apple
  >
-   <View style={{ position: 'absolute', top: 10, width: '100%' }}>
+   <View style={{alignSelf:'flex-start', position: 'absolute', top: 10, width: '38%',height:'15%' }}>
   <Input
- style={styles.input}
- 
- 
- placeholder={'Search'}
+  label='Radius:'
+ //style={styles.input}
+ placeholder={'How many KM?'}
  placeholderTextColor='black'
+ keyboardType='numeric'
+ getValue={(value) => setDistance(value*1000/2)}
  />
 </View>
+<Marker 
+coordinate={{latitude: region.latitude, longitude: region.longitude }}
+title="I'm Here !"/>
 <Marker 
 coordinate={pin}
 pinColor='blue'
@@ -65,12 +121,13 @@ onDragEnd={(e)=>{
 //image={require('../assets/icon.png')}
 >
 <Callout>
-  <Text>Im Over Here!</Text>
+  <Text>I Want to be Here!</Text>
 </Callout>
 
 </Marker>
-<Circle center={pin} 
-radius={1000}
+<Circle
+center={pin} 
+radius={distance}
 />
 </MapView>
 
@@ -88,14 +145,14 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
-  input:{
-    borderRadius: 10,
-     margin: 10,
-     color: '#000',
-      borderColor: 'black',
-     borderWidth: 1,
-     height: 9,
-     paddingHorizontal: 10,
-     fontSize: 18,
-  },
+  // input:{
+  //   borderRadius: 10,
+  //    margin: 10,
+  //    color: '#000',
+  //     borderColor: 'black',
+  //    borderWidth: 1,
+  //    height: 9,
+  //    paddingHorizontal: 10,
+  //    fontSize: 18,
+  // },
 });
