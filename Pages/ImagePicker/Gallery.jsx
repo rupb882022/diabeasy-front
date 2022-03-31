2b11b38cb102e7f13bc36cc3a7312100ec69817b
tal farkash <tal_farkash@walla.com>
2022-03-31 14:02:38 +0300

    favorit and camera

--- Pages/ImagePicker/Gallery.jsx
+++ Pages/ImagePicker/Gallery.jsx
@@ -33,7 +33,7 @@ export default function Gallery(props) {
   }, [])
 
 
-
+console.log("userDetails",userDetails);
     
     
   //choose *only* picture
@@ -61,9 +61,6 @@ ImgUpload(`${image}`
 }
 
 
-
-
-//#Nir check (!Request.Content.IsMimeMultipartContent()) in C#
 const ImgUpload = (imgUri, picName) => {
   let dataI = new FormData();
   dataI.append('picture', {
@@ -76,6 +73,9 @@ const ImgUpload = (imgUri, picName) => {
     body: dataI,
 
     }
+    
+
+
      setLoading(true)
     fetch(upiUrl+"uploadpicture", config)
     .then((res) => {
@@ -87,16 +87,19 @@ const ImgUpload = (imgUri, picName) => {
     if (responseData != "err") {
       
       console.log("LOGpicName1=> ",picName)
-    let picNameWOExt = picName.substring(0, picName.indexOf("."));
-    let imageNameWithGUID = responseData.substring(responseData.indexOf(picNameWOExt),
-    responseData.indexOf(".jpg") + 4);
-    console.log('new pic name=> ',imageNameWithGUID);
+
     console.log("img uploaded successfully!");   
     setLoading(false)
    setDonePicture(true)
    setShow(false)
    console.log('DONE!');
-   
+   if(picName.startsWith("profileDoctor")||picName.startsWith("profilePatient")){
+    setUserDetails({
+      id:userDetails.id,
+      name:userDetails.name,
+      image:picName
+    })
+   }
     }
     else {alert('error uploding ...');
     setLoading(false)
