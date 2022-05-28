886063c4faf8a6cfb2f27fc84dd428e239330bee
tal farkash <tal_farkash@walla.com>
2022-05-28 16:23:59 +0300

    fix bugs

--- Pages/Recommandtion/HipoFood.jsx
+++ Pages/Recommandtion/HipoFood.jsx
@@ -1,77 +1,116 @@
-import { View, Text ,StyleSheet,Image,Dimensions} from 'react-native'
-import React from 'react'
-import {ImageUri} from '../../Routes/Url'
+import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { ImageUri } from '../../Routes/Url'
 import moment from 'moment';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function HipoFood(props) {
 
-  const window =Dimensions.get('window');
-  const {hide=false,next_blood_sugar_level,totalCarbs,food_name,Unit_name,image,date_time,blood_sugar_level,amount,count}=props
+  const window = Dimensions.get('window');
+  // const {hide=false,next_blood_sugar_level,totalCarbs,food_name,Unit_name,image,date_time,blood_sugar_level,amount,count}=props
+  const { times_eaten,position, hide = false, next_blood_sugar_level, totalCarbs, date_time, blood_sugar_level, Food } = props
+  
+  const [food, setFood] = useState(Food && Food[0]);
+
+  let image=food?food.image ? food.image.includes("http") ? food.image : ImageUri + food.image : ImageUri + 'emptyFoodPhoto.JPG':'' ;
+  
+
+  const next_item = (side) => {
+
+    let index=0;
+     Food.forEach((x, i) => {
+      if (food.foodId == x.foodId) {
+        index=i;
+      }
+    })
+    console.log("index",index)
+    if (side == "left" && index == 0) {
+      setFood(Food[Food.length - 1])
+    }else if(side == "left" && index != 0){
+      setFood(Food[index - 1])
+    }else if(side == "right" && index == 0){
+      setFood(Food[index + 1])
+    }else{
+      setFood(Food[0])
+    }
+  }
   return (
-    <View style={styles.container(Math.floor(window.height-window.height/10*7.5),hide)}>
-  {hide?<></>: <> 
-  <Text style={styles.count}>{count}</Text>
-   <Text style={styles.title}>{amount} {Unit_name} {food_name}</Text>
-      <Image style={styles.image}  source={{ uri: image ? image.includes("http")?image:ImageUri +image : ImageUri + 'emptyFoodPhoto.JPG' }} />
-      <Text style={styles.content}>date: {moment(date_time).format('DD/MM/YYYY')}</Text>
-      <Text style={styles.content}>sugar level: {blood_sugar_level}</Text>
-      <Text style={styles.content}>next sugar level: {next_blood_sugar_level}</Text>
-      <Text style={styles.content}>carbohydrates: {totalCarbs}</Text>
-      </>  }
+    <View style={styles.container(Math.floor(window.height - window.height / 10 * 7.5), hide)}>
+      {hide ? <></> : <>
+        <Text style={styles.count}>{position}</Text>
+        <Text style={styles.title}>{food.amount} {food.unitName} {food.FoodName}</Text>
+        <Image style={styles.image} source={{ uri:image }} />
+        <Text style={styles.content}>date: {moment(date_time).format('DD/MM/YYYY')}</Text>
+        <Text style={styles.content}>sugar level: {blood_sugar_level}</Text>
+        <Text style={styles.content}>next sugar level: {next_blood_sugar_level}</Text>
+        <Text style={styles.content}>carbohydrates: {totalCarbs}</Text>
+        <Text style={styles.content}>times eaten: {times_eaten}</Text>
+      </>}
+      {Food && Food.length > 1 &&
+        <View style={{ position: 'absolute', flexDirection: 'row', top: '44%', left: '5%', }}>
+          <TouchableOpacity onPress={() => { next_item("left"); }}>
+            <AntDesign name="arrowleft" size={30} color="#767676" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { next_item("right"); }} style={{ left: '290%' }}>
+            <AntDesign name="arrowright" size={30} color="#767676" />
+          </TouchableOpacity>
+        </View>
+      }
     </View>
   )
 }
 const styles = StyleSheet.create({
-  container:(height,hide)=>{
-    if(hide){
-      return{
-        backgroundColor:'transparent',
-        flexDirection:'colum',
+  container: (height, hide) => {
+    if (hide) {
+      return {
+        backgroundColor: 'transparent',
+        flexDirection: 'colum',
         flexBasis: '50%',
       }
     }
-    return{flex:1,
-     flexDirection:'colum',
-     flexBasis: '45%',
-     marginHorizontal:'2%',
-     marginBottom:'4%',
-    height:height,
-    // justifyContent:'center',
-    backgroundColor:'white',
-    shadowOffset: {
-      width: -2,
-      height: 1
-    },
-    shadowColor:'#727272',
-    shadowOpacity:1,
-    padding:'1%',
-    justifyContent:'space-evenly',
-    
-  }
-},image:{
+    return {
+      flex: 1,
+      flexDirection: 'colum',
+      flexBasis: '45%',
+      marginHorizontal: '2%',
+      marginBottom: '4%',
+      height: height,
+      // justifyContent:'center',
+      backgroundColor: 'white',
+      shadowOffset: {
+        width: -2,
+        height: 1
+      },
+      shadowColor: '#727272',
+      shadowOpacity: 1,
+      padding: '1%',
+      justifyContent: 'space-evenly',
+
+    }
+  }, image: {
     width: '100%',
     height: '45%',
-    justifyContent:'center',
+    justifyContent: 'center',
     resizeMode: 'contain',
   },
-  count:{
-    position:'absolute',
-    fontSize:40,
-    top:'30%',
-    left:'5%',
-    color:'#e6e6e69c'
+  count: {
+    position: 'absolute',
+    fontSize: 40,
+    top: '23%',
+    left: '5%',
+    color: '#e6e6e69c'
   },
-  title:{
-    textAlign:'center',
- paddingBottom:'2%',
- fontSize:15,
- fontWeight:'600',
-//  bottom:'2%'
+  title: {
+    textAlign: 'center',
+    paddingBottom: '2%',
+    fontSize: 15,
+    fontWeight: '600',
+    //  bottom:'2%'
   },
-  content:{
-    textAlign:'auto',
-    color:'#818080',
-    top:'1%',
-    paddingLeft:'5%'
+  content: {
+    textAlign: 'auto',
+    color: '#818080',
+    top: '1%',
+    paddingLeft: '5%'
   }
 })
--- Pages/Recommandtion/HipoRec.jsx
+++ Pages/Recommandtion/HipoRec.jsx
@@ -1,31 +1,32 @@
-import { View, Text, StyleSheet, SafeAreaView, ScrollView, StatusBar } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, ScrollView, StatusBar } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import HipoFood from './HipoFood';
 import Button from '../../CTools/Button';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function HipoRec({ route, navigation }) {
 
   let hipoFood = route.params && route.params.hipoFood ? route.params.hipoFood : '';
 
   const [hipo_Food, sethipoFood] = useState(hipoFood);
+  const [list, setList] = useState();
   const [color, setColor] = useState('date');
   let Dcolor = '#646363cc';
-  const order_by = (val) => {
-let temp=hipo_Food;
-let Tcolor='';
+  const order_by = async(val) => {
+    setList([])
+    let temp = hipo_Food;
+    let Tcolor = '';
     switch (val) {
       case 'date':
-        temp.sort((a, b) => b.date_time - a.date_time);
-        Tcolor='date'
+        temp.sort((a, b) => new Date(b.date_time) - new Date(a.date_time));
+        Tcolor = 'date'
         break;
-      case 'Times':
-        temp.sort((a, b) => b.count - a.count);
-        Tcolor='Times'
+      case 'times_eaten':
+        temp.sort((a, b) => b.times_eaten - a.times_eaten);
+        Tcolor = 'times_eaten'
         break;
       case 'carbs':
         temp.sort((a, b) => b.totalCarbs - a.totalCarbs);
-        Tcolor='carbs'
+        Tcolor = 'carbs'
         break;
 
     }
@@ -33,28 +34,35 @@ let Tcolor='';
     setColor(Tcolor)
   }
 
-  return (
-    <>
+
+  useEffect(() => {
+    hipo_Food && setList(hipo_Food)
+  }, [color])
+  return (<>
+    <View style={{ flex: 0.95 }}>
       <Text style={styles.title}>These foods helped you last time</Text>
       <View style={{ flexDirection: 'row', borderColor: 'white', borderWidth: 1, width: '91.5%', alignSelf: 'center', backgroundColor: '#d7fcf49c', }}>
         <Text style={styles.orderBy}>order by:</Text>
-        <TouchableOpacity onPress={() => {order_by('date'); }} style={styles.button}><Text style={styles.text(color=='date'?'#00a6a685':Dcolor)}>Date</Text></TouchableOpacity>
-        <TouchableOpacity onPress={() => {order_by('Times'); }} style={styles.button}><Text style={styles.text(color=='Times'?'#00a6a685':Dcolor)}>Times eaten</Text></TouchableOpacity>
-        <TouchableOpacity onPress={() => {order_by('carbs'); }} style={styles.button}><Text style={styles.text(color=='carbs'?'#00a6a685':Dcolor)}>Total carbs</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => { order_by('date'); }} style={styles.button}><Text style={styles.text(color == 'date' ? '#00a6a685' : Dcolor)}>Date</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => { order_by('times_eaten'); }} style={styles.button}><Text style={styles.text(color == 'times_eaten' ? '#00a6a685' : Dcolor)}>Times eaten</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => { order_by('carbs'); }} style={styles.button}><Text style={styles.text(color == 'carbs' ? '#00a6a685' : Dcolor)}>Total carbs</Text></TouchableOpacity>
       </View>
       <ScrollView contentContainerStyle={styles.container}>
-        {hipoFood && hipoFood.map((x, i) => {
+        {list && list.map((x, i) => {
           return <HipoFood
             key={i}
-            amount={x.amount}
+            position={i + 1}
+            // amount={x.amount}
             blood_sugar_level={x.blood_sugar_level}
             next_blood_sugar_level={x.blood_sugar_level_2H}
             date_time={x.date_time}
-            image={x.image}
-            Unit_name={x.UnitName}
-            food_name={x.FoodName}
+            Food={x.food}
+            times_eaten={x.times_eaten}
+            // image={x.image}
+            // Unit_name={x.UnitName}
+            // food_name={x.FoodName}
             totalCarbs={x.totalCarbs}
-            count={x.count}
+          // count={x.count}
           />
         })}
         {hipoFood && hipoFood.length % 2 !== 0 &&
@@ -63,27 +71,30 @@ let Tcolor='';
           />
         }
       </ScrollView>
-      <Button
-        text='back'
-        width={6}
-        height={3}
-        radius={8}
-        alignItems='center'
-        justifyContent='center'
-        onPress={() => { navigation.goBack() }}
-      />
-    </>
+
+    </View>
+    <Button
+      text='back'
+      width={6}
+      height={3}
+      flex={0.1}
+      radius={8}
+      alignItems='center'
+      justifyContent='center'
+      onPress={() => { navigation.goBack() }}
+    /></>
   )
 }
 
 const styles = StyleSheet.create({
 
   container: {
-    // flex: 1,
+    //  flex: 5,
     flexDirection: 'row',
     flexWrap: 'wrap',
     padding: 10,
-    width: '100%'
+    width: '100%',
+    // height:'60%'
   }, title: {
     alignSelf: 'center',
     marginTop: '18%',
